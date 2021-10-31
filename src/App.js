7716fcffd00b15f80274241b696a1e98b21a10a1
petk1234 <petkun8034@gmail.com>
2021-10-31 11:30:30 +0200

    some changes x2

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default class App extends Component{
     let localStorageInput = localStorage.getItem('contacts');
 
     
+    
     if(localStorageInput !== null){
       this.setState( prevState => (
         {contacts: prevState.contacts.concat(JSON.parse(localStorageInput))}
