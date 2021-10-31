383a2b164159a8e9a3b8ec79eb87061a6d9ad78b
petk1234 <petkun8034@gmail.com>
2021-10-31 11:27:33 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default class App extends Component{
 
   componentDidMount(){
     let localStorageInput = localStorage.getItem('contacts');
+
     
     if(localStorageInput !== null){
       this.setState( prevState => (
